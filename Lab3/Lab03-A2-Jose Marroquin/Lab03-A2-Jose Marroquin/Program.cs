2f99133c7cbf0d9357b8c8b207fa8f70a7653962
davidmarrok <davidmarro123rd@gmail.com>
2026-02-16 01:32:33 -0600

    L3_JDMN1145026
    lab 3

---
+++ Lab3/Lab03-A2-Jose Marroquin/Lab03-A2-Jose Marroquin/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nombre;
+string curso;
+Console.Write("Ingrese su nombre ");
+nombre = Console.ReadLine();
+Console.Write("Ingrese el nombre del curso ");
+curso = Console.ReadLine();
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.Write("Hola " + nombre);
+Console.WriteLine(", esperamos que ganes el curso de " + curso);
+Console.WriteLine("Te deseamos éxito en tus actividades.");
+Console.WriteLine(" ");
+Console.WriteLine("Recuerda las palabras de CR7 \"Obsession beats talent\".");
+Console.WriteLine("Presione una tecla para salir* ");
+Console.ReadKey();
